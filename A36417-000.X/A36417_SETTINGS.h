e742b98885095fab10a086bb7cd043497d47d31e
dparkermit <dparkermit@yahoo.com>
2016-07-16 11:17:55 -0700

    A36417-252 Version 3.1.1
    Update the trip points and time

--- A36417-000.X/A36417_SETTINGS.h
+++ A36417-000.X/A36417_SETTINGS.h
@@ -12,14 +12,14 @@
 #define ION_PUMP_VOLTAGE_SCALE_FACTOR            .0763              //1V per 1kV
 #define ION_PUMP_VOLTAGE_OVER_TRIP_POINT         4300               //3.3kV
 #define ION_PUMP_VOLTAGE_UNDER_TRIP_POINT        2500               //2.9kV
-#define ION_PUMP_VOLTAGE_ABSOLUTE_TRIP_TIME      2000                 // This is in 10ms Units
+#define ION_PUMP_VOLTAGE_ABSOLUTE_TRIP_TIME      5000                 // This is in 10ms Units
 
 #define ION_PUMP_CURRENT_SCALE_FACTOR            .50863            
 #define ION_PUMP_CURRENT_OVER_TRIP_POINT         5000               // 50uA this is in 10nA units
 #define ION_PUMP_CURRENT_UNDER_TRIP_POINT        0                  //
-#define ION_PUMP_CURRENT_ABSOLUTE_TRIP_TIME      100                 // This is in 10ms Units
+#define ION_PUMP_CURRENT_ABSOLUTE_TRIP_TIME      200                 // This is in 10ms Units
 
-#define CURRENT_LOWER_THRESHOLD                  1000              // 10 uA - this is 10nA units
+#define CURRENT_LOWER_THRESHOLD                  2000              // 20 uA - this is 10nA units
 
 #define TARGET_CURRENT_SCALE_FACTOR              0.0763/5           //placeholder
 #define TARGET_CURRENT_OVER_TRIP_POINT           555                //placeholder
